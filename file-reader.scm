907dd162f08710754134eab61b69be51d1397c2c
Geoffrey Gilmore <tkdmaster123@gmail.com>
2016-05-03 18:07:42 -0400

    valid->valid?, begin-tasks->task-separator, task-separator is an arg in read-tasks

--- file-reader.scm
+++ file-reader.scm
@@ -1,12 +1,17 @@
-(define reader:begin-tasks "--BEGIN--")
 
+;;this is the divider in the text file - used
+;;to separate schedule options from tasks 
+(define reader:task-separator "--BEGIN--")
+
+
+;;reads a text file
 (define (reader:read-tasks file-name delimiter)
 
   (define error? symbol?)
   
   (let lp ((out '())
 	   (rest (open-input-file file-name))
-	   (valid parser:valid:options?)
+	   (valid? parser:valid:options?)
 	   (number 0))
 
 
@@ -19,18 +24,18 @@
 	    
 					;blank line - skip
 	    ((eq? 0 (string-length (string-trim line)))
-	     (lp out rest valid (+ 1 number)))
+	     (lp out rest valid? (+ 1 number)))
 
 					;are we reading tasks now?
-	    ((equal? `(,reader:begin-tasks)
+	    ((equal? `(,reader:task-separator)
 		  (parser:readline line delimiter))
 	     (lp out rest parser:valid:task? (+ 1 number)))
 
 	    (else
-	     (let ((new (valid line delimiter)))
+	     (let ((new (valid? line delimiter)))
 	       (if (error? new)
 		   `(reader-error ,number ,new ,line)
-		   (lp (cons new out) rest  valid (+ 1 number)))))))))
+		   (lp (cons new out) rest  valid? (+ 1 number)))))))))
 
 
 
