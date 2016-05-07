86ac2816af8af339af254f1f850004cf9f7dc651
John J O'Sullivan <johno@mit.edu>
2016-05-07 15:50:27 -0400

    Added hash table to store options, merging into the rest of code.

--- scheduler.scm
+++ scheduler.scm
@@ -7,12 +7,29 @@
 (define current-time
   ('instant (universal-time->local-decoded-time (get-universal-time))))
 
-(define hours-per-day 8)
-(define tool-duration ('duration 0 3 0))
-(define punt-duration ('duration 0 3 15))
+(define options (make-eq-hash-table))
+(hash-table/put! options task-hours-per-day 8)
+(hash-table/put! options monday-hours 8)
+(hash-table/put! options tuesday-hours 8)
+(hash-table/put! options wednesday-hours 8)
+(hash-table/put! options thursday-hours 8)
+(hash-table/put! options friday-hours 8)
+(hash-table/put! options saturday-hours 8)
+(hash-table/put! options sunday-hours 8)
+(hash-table/put! options tool-duration ('duration 0 3 0))
+(hash-table/put! options punt-duration ('duration 0 0 15))
 
-(define (set-hours-per-day value)
-  (set hours-per-day value))
+(define (set-hours-per-day-of-week string) 
+
+  )
+
+(define (set-task-hours-per-day string)
+  
+  )
+
+(define (set-work-durations string)
+
+  )
 
 (define completed-tasks (make-eq-hash-table)
 (define available-tasks (make-eq-hash-table)
@@ -32,7 +49,10 @@
 	(else (set available-tasks (hash-table/put! available-tasks id new-task)))))
 
 (define (select-task available-tasks completed-tasks)
-  
+  (let ((most-urgent 
+	 (hash-table/get available-tasks 
+			 (car (hash-table/key-list available-tasks))))
+	(time-remaining ))
   )
 
 (define (compute-time-remaining task current-time)
