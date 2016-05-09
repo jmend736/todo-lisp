eaaa72256445cdf57a180f4022381c6e0592dd71
John J O'Sullivan <johno@mit.edu>
2016-05-09 04:58:30 -0400

    Made two more sample files with more dependencies, added input- and output-filenames to todo-lisp.

--- todo-lisp.scm
+++ todo-lisp.scm
@@ -1,15 +1,14 @@
 ;; This is the main file which provides a function to specify a filename and
 ;; output a schedule.  It also automatically runs itself with "sample.txt".
 
-(define (todo-lisp filename)
-  (define parsed-file (reader:read-tasks filename "#!" reader:task-separator))
-  (pp parsed-file)
+(define (todo-lisp input-filename output-filename)
+  (define parsed-file 
+    (reader:read-tasks input-filename "#!" reader:task-separator))
   (sched:set-options (car parsed-file))
   (sched:make-tasks (cdr parsed-file))
   (define final-schedule (sched:get-schedule))
-  (pp final-schedule))
-    ; (define format)
-    ; ((d:factory format) final-schedule)))
+  (write-dot-file 
+   ((d:factory final-format) final-schedule output-filename) output-filename)
+  (d:make-time-subgraph final-schedule))
 
 
-(todo-lisp "sample.txt")
